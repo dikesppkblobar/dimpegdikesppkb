a57135c0989fe2e4f2b69ccb37d9ef6440ae30b4
dikesppkblobar <dikesppkblobar@gmail.com>
2026-06-25 15:40:49 +0700

    Create test-require.ts

---
+++ test-require.ts
@@ -0,0 +1,8 @@
+{
+  "name": "SAPA pegawai Dikes PPKB Lombok Barat",
+  "description": "Sapa Pegawai Dikes PPKB: Sistem Arsip, Pemberitahuan, & Analisis Kepegawaian Dinas Kesehatan Pengendalian Penduduk dan Keluarga Berencana Kab. Lombok Barat.",
+  "requestFramePermissions": [],
+  "majorCapabilities": [
+    "MAJOR_CAPABILITY_SERVER_SIDE_GEMINI_API"
+  ]
+}
